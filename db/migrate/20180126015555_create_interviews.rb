dba8ed7d17efa56c87cdd0e4e59f303f13b5c9cf
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-01-30 08:44:12 +0000

    migrate production database

--- db/migrate/20180126015555_create_interviews.rb
+++ db/migrate/20180126015555_create_interviews.rb
@@ -2,7 +2,7 @@ class CreateInterviews < ActiveRecord::Migration[5.1]
   def change
     create_table :interviews do |t|
       t.references :user, foreign_key: true
-      t.datetime :schedule
+      t.datetime :schedule # datetime 
       t.boolean :propriety
 
       
